a35d02033e9366407ce4cdb4be590b4fd2741dcc
faizaldara <agusbukanorangjahat@gmail.com>
2025-09-20 00:12:53 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // Togle class active
 const navbarNav = document.querySelector('.navbar-nav');
 // hamburger menu di klik
-document.querySelector('#humbergermenu').onclick = ( ) => {
+document.querySelector('#hamburgermenu').onclick = ( ) => {
 	navbarNav.classList.toggle('active');
 };
 
 // klick diluar sidebar
-const hamberger = document.querySelector('#humbergermenu');
+const hamburger = document.querySelector('#hamburgermenu');
 
-document.addEventlListener('click' , function (e) {
-	if (!hamberger.contains(e.target) && !navbarNav.contains(e.target)) {
+document.addEventlListener('click', function (e) {
+	if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
 		navbarNav.classList.remove('active');
 	}
 
